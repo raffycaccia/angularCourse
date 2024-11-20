02adecb5c837ec712b4eeb81ec1438f297bbba2c
unknown <r.caccia@outlook.com>
2024-11-20 18:48:29 +0100

    solution 4 test 1

---
+++ module4-solution/js/routes.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+
+    angular.module('MenuApp')
+        .config(RoutesConfig);
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'templates/home.template.html'
+            })
+            .state('categories', {
+                url: '/categories',
+                templateUrl: 'templates/categories.template.html',
+                controller: 'CategoriesController as categoriesCtrl',
+                resolve: {
+                    items: ['MenuDataService', function(MenuDataService) {
+                        return MenuDataService.getAllCategories();
+                    }]
+                }
+            })
+            .state('items', {
+                url: '/items/{categoryShortName}',
+                templateUrl: 'templates/items.template.html',
+                controller: 'ItemsController as itemsCtrl',
+                resolve: {
+                    items: ['MenuDataService', '$stateParams', function(MenuDataService, $stateParams) {
+                        return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+                    }]
+                }
+            });
+    }
+})();
