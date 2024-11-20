3e7298ffb2c4887b9eb2d2a0e790dab3b16094b9
unknown <r.caccia@outlook.com>
2024-11-20 19:20:57 +0100

    solution 4 rewrite2

--- module4-solution/app/routes.js
+++ module4-solution/app/routes.js
@@ -1,41 +1,41 @@
 (function () {
-    'use strict';
-  
-    angular.module('MenuApp')
-      .config(RoutesConfig);
-  
-    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-    function RoutesConfig($stateProvider, $urlRouterProvider) {
-      // Redirect to home page if no other URL matches
-      $urlRouterProvider.otherwise('/');
-  
-      // Set up UI states
-      $stateProvider
-        .state('home', {
-          url: '/',
-          templateUrl: 'templates/home.template.html'
-        })
-        .state('categories', {
-          url: '/categories',
-          templateUrl: 'templates/categories.template.html',
-          controller: 'CategoriesController as categoriesCtrl',
-          resolve: {
-            categories: ['MenuDataService', function (MenuDataService) {
-              return MenuDataService.getAllCategories();
-            }]
-          }
-        })
-        .state('items', {
-          url: '/items/{categoryShortName}',
-          templateUrl: 'templates/items.template.html',
-          controller: 'ItemsController as itemsCtrl',
-          resolve: {
-            items: ['$stateParams', 'MenuDataService',
-              function ($stateParams, MenuDataService) {
-                return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
-              }]
-          }
-        });
-    }
-  })();
-  
+  'use strict';
+
+  angular.module('MenuApp')
+    .config(RoutesConfig);
+
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+    // Default route
+    $urlRouterProvider.otherwise('/');
+
+    // *** Set up UI states ***
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'templates/home.template.html'
+      })
+
+      .state('categories', {
+        url: '/categories',
+        templateUrl: 'templates/categories.template.html',
+        controller: 'CategoriesController as categoriesCtrl',
+        resolve: {
+          categories: ['MenuDataService', function (MenuDataService) {
+            return MenuDataService.getAllCategories();
+          }]
+        }
+      })
+
+      .state('items', {
+        url: '/items/{categoryShortName}',
+        templateUrl: 'templates/items.template.html',
+        controller: 'ItemsController as itemsCtrl',
+        resolve: {
+          items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+            return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+          }]
+        }
+      });
+  }
+})();
