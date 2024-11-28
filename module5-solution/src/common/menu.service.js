41ad34add951839b17edef9e6f4b174eb8079689
unknown <r.caccia@outlook.com>
2024-11-28 22:01:52 +0100

    upload

--- module5-solution/src/common/menu.service.js
+++ module5-solution/src/common/menu.service.js
@@ -15,15 +15,23 @@ function MenuService($http, ApiPath) {
     });
   };
 
-
   service.getMenuItems = function (category) {
     return $http.get(ApiPath + '/menu_items/' + category + '.json').then(function (response) {
       return response.data;
     });
   };
+  
+  service.getMenuItem = function (shortName) {
+    return $http.get(ApiPath + '/menu_items/' + shortName.toUpperCase() + '.json')
+      .then(function (response) {
+        return response.data;
+      })
+      .catch(function (error) {
+        console.error("Error fetching menu item:", error);
+        throw error;
+      });
+  };
 
 }
-
-
 
 })();
